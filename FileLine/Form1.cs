9fc80ef4e0554d5fa81d7c82f7cad61c614d5c69
LZ <LZ@ALIEN>
2016-12-09 12:40:02 +0800

    120901

--- FileLine/Form1.cs
+++ FileLine/Form1.cs
@@ -2,9 +2,13 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,9 +16,77 @@ namespace FileLine
 {
     public partial class Form1 : Form
     {
+        //server
+        private static byte[] result = new byte[1024];
+        private static int myProt = 8885;   //端口
+        static Socket serverSocket;
+
+        //client
+
+
+
         public Form1()
         {
             InitializeComponent();
+            socketStart();
         }
+
+        //服务器方法
+        #region
+        /// <summary>
+        /// 开启服务器监听
+        /// </summary>
+        private void socketStart()
+        {
+            var ipAd =  new tools().GetAddressIP();
+            //服务器IP地址
+            IPAddress ip = IPAddress.Parse(ipAd);
+            serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            serverSocket.Bind(new IPEndPoint(ip, myProt));  //绑定IP地址：端口
+            serverSocket.Listen(10);    //设定最多10个排队连接请求
+            Debug.WriteLine("启动监听{0}成功", serverSocket.LocalEndPoint.ToString());
+            //通过Clientsoket发送数据
+            Thread myThread = new Thread(ListenClientConnect);
+            myThread.Start();
+        }
+        /// <summary>
+        /// 监听客户端连接
+        /// </summary>
+        private static void ListenClientConnect()
+        {
+            while (true)
+            {
+                Socket clientSocket = serverSocket.Accept();
+                clientSocket.Send(Encoding.ASCII.GetBytes("Server Say Hello"));
+                Thread receiveThread = new Thread(ReceiveMessage);
+                receiveThread.Start(clientSocket);
+            }
+        }
+        /// <summary>
+        /// 接收消息
+        /// </summary>
+        /// <param name="clientSocket"></param>
+        private static void ReceiveMessage(object clientSocket)
+        {
+            Socket myClientSocket = (Socket)clientSocket;
+            while (true)
+            {
+                try
+                {
+                    //通过clientSocket接收数据
+                    int receiveNumber = myClientSocket.Receive(result);
+                    Debug.WriteLine("接收客户端{0}消息{1}", myClientSocket.RemoteEndPoint.ToString(), Encoding.ASCII.GetString(result, 0, receiveNumber));
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine(ex.Message);
+                    myClientSocket.Shutdown(SocketShutdown.Both);
+                    myClientSocket.Close();
+                    Debug.WriteLine("error");
+                    break;
+                }
+            }
+        }
+        #endregion
     }
 }
---
+++ FileLine/tools.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileLine
+{
+    class tools
+    {
+        /// <summary>
+        /// 获取本地IP地址信息
+        /// </summary>
+        public string GetAddressIP()
+        {
+            ///获取本地的IP地址
+            string AddressIP = string.Empty;
+            foreach (IPAddress _IPAddress in Dns.GetHostEntry(Dns.GetHostName()).AddressList)
+            {
+                if (_IPAddress.AddressFamily.ToString() == "InterNetwork")
+                {
+                    AddressIP = _IPAddress.ToString();
+                }
+            }
+            return AddressIP;
+        }
+    }
+}
